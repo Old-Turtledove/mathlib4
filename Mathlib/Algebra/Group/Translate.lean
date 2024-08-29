07c13b62f17d7169ff1eb88e5115de003f74c792
Yaël Dillies <yael.dillies@gmail.com>
2024-08-29 08:03:52 +0000

    Bhavik's comments

--- Mathlib/Algebra/Group/Translate.lean
+++ Mathlib/Algebra/Group/Translate.lean
@@ -19,13 +19,15 @@ This file defines the translation of a function from a group by an element of th
 open Function Set
 open scoped Pointwise
 
-variable {ι α β M G H R : Type*} [AddCommGroup G]
+variable {ι α β M G H : Type*} [AddCommGroup G]
 
 /-- Translation of a function in a group by an element of that group.
 `τ a f` is defined as `x ↦ f (x - a)`.  -/
 def translate (a : G) (f : G → α) : G → α := fun x ↦ f (x - a)
 
-@[inherit_doc] notation "τ " => translate
+@[inherit_doc] scoped[translate] notation "τ " => translate
+
+open scoped translate
 
 @[simp] lemma translate_apply (a : G) (f : G → α) (x : G) : τ a f x = f (x - a) := rfl
 @[simp] lemma translate_zero (f : G → α) : τ 0 f = f := by ext; simp
@@ -41,17 +43,19 @@ lemma translate_add' (a b : G) (f : G → α) : τ (a + b) f = τ b (τ a f) := 
 lemma translate_comm (a b : G) (f : G → α) : τ a (τ b f) = τ b (τ a f) := by
   rw [← translate_add, translate_add']
 
+-- We make `simp` push the `τ` outside
 @[simp] lemma comp_translate (a : G) (f : G → α) (g : α → β) : g ∘ τ a f = τ a (g ∘ f) := rfl
 
 @[simp]
 lemma translate_smul_right [SMul H α] (a : G) (f : G → α) (c : H) : τ a (c • f) = c • τ a f := rfl
 
+@[simp] lemma translate_zero_right [Zero α] (a : G) : τ a (0 : G → α) = 0 := rfl
+lemma translate_add_right [Add α] (a : G) (f g : G → α) : τ a (f + g) = τ a f + τ a g := rfl
+lemma translate_sub_right [Sub α] (a : G) (f g : G → α) : τ a (f - g) = τ a f - τ a g := rfl
+lemma translate_neg_right [Neg α] (a : G) (f : G → α) : τ a (-f) = -τ a f := rfl
+
 section AddCommMonoid
 variable [AddCommMonoid M]
-
-@[simp] lemma translate_zero_right (a : G) : τ a (0 : G → M) = 0 := rfl
-
-lemma translate_add_right (a : G) (f g : G → M) : τ a (f + g) = τ a f + τ a g := rfl
 
 lemma translate_sum_right (a : G) (f : ι → G → M) (s : Finset ι) :
     τ a (∑ i in s, f i) = ∑ i in s, τ a (f i) := by ext; simp
@@ -64,15 +68,12 @@ end AddCommMonoid
 section AddCommGroup
 variable [AddCommGroup H]
 
-lemma translate_sub_right (a : G) (f g : G → H) : τ a (f - g) = τ a f - τ a g := rfl
-lemma translate_neg_right (a : G) (f : G → H) : τ a (-f) = -τ a f := rfl
-
 @[simp] lemma support_translate (a : G) (f : G → H) : support (τ a f) = a +ᵥ support f := by
   ext; simp [mem_vadd_set_iff_neg_vadd_mem, sub_eq_neg_add]
 
 end AddCommGroup
 
-variable [CommRing R]
+variable [CommMonoid M]
 
-lemma translate_prod_right (a : G) (f : ι → G → R) (s : Finset ι) :
+lemma translate_prod_right (a : G) (f : ι → G → M) (s : Finset ι) :
     τ a (∏ i in s, f i) = ∏ i in s, τ a (f i) := by ext; simp
